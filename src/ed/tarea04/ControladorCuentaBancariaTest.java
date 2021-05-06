35935067d411f74f405f63babecd4e00330edb1e
Cherry708 <aluser8241@ieselcaminas.org>
2021-05-06 20:59:52 +0200

    Tarea 9. Tarea después de Pascua. No se entiende el propósito del test, preguntar en clase. Hace falta modificar el código original?

--- src/ed/tarea04/ControladorCuentaBancariaTest.java
+++ src/ed/tarea04/ControladorCuentaBancariaTest.java
@@ -15,9 +15,42 @@ public class ControladorCuentaBancariaTest {
     CuentaBancaria pruebaTest = new CuentaBancaria("Rubén Serrano", "00491500051234567892");
 
     @Test
-    public void operacionesCuentaTestIngresarValidoTest() throws Exception{
+    public void operacionesCuentaTestIngresarValidoTest(){
         pruebaTest.ingresar(10000);
         Assert.assertEquals(10000,pruebaTest.getSaldo(),0.0);
+    }
+
+    @Test
+    public void operacionesCuentaTestIngresarNegativoTest() throws Exception{
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("No es posible realizar la operación. La cantidad a ingresar debe ser un número positivo. ");
+        pruebaTest.ingresar(-1);
+        Assert.assertEquals(0,pruebaTest.getSaldo(),0.0);
+    }
+
+    @Test
+    public void operacionesCuentaTestRetirarValidoTest(){
+        pruebaTest.setSaldo(1);
+        pruebaTest.retirar(1);
+        Assert.assertEquals(0,pruebaTest.getSaldo(),0.0);
+    }
+
+    @Test
+    public void operacionesCuentaTestRetirarSaldoInsuficienteTest() throws Exception{
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("No es posible realizar la operación. El saldo es inferior a la cantidad a retirar. ");
+        pruebaTest.setSaldo(0);
+        pruebaTest.retirar(1);
+        Assert.assertEquals(0,pruebaTest.getSaldo(),0.0);
+    }
+
+    @Test
+    public void operacionesCuentaTestRetirarSaldoNegativo() throws Exception{
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("No es posible realizar la operación. La cantidad a retirar debe ser un número positivo. ");
+        pruebaTest.setSaldo(0);
+        pruebaTest.retirar(-1);
+        Assert.assertEquals(0, pruebaTest.getSaldo(),0.0);
     }
 
 }
